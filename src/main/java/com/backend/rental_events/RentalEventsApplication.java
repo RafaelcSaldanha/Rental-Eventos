1d9ef635d9b543fa1b6192533a2f992e39adaaa2
RafaelcSaldanha <saldanhar494@gmail.com>
2026-08-25 10:12:34 -0300

    Criação do Backend

---
+++ src/main/java/com/backend/rental_events/RentalEventsApplication.java
@@ -0,0 +1,14 @@
+package com.backend.rental_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalEventsApplication.class, args);
+		System.out.println("hello world!");
+	}
+
+}
---
+++ src/test/java/com/backend/rental_events/RentalEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.rental_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
